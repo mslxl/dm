737caa5f263c35c3950a1d8b0b5d9e87523583ff
mslxl <i@mslxl.com>
2023-02-06 19:56:40 +0800

    fix(cli): program crash when no args input

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ mod platform;
 mod task;
 mod ui;
 
-use clap::command;
-use miette::Result;
+use clap::{command, Command};
+use miette::{IntoDiagnostic, Result};
 use rust_i18n::t;
 
 rust_i18n::i18n!("locales");
@@ -18,20 +18,28 @@ async fn apply_locales() {
     rust_i18n::set_locale(locale);
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    apply_locales().await;
-    let matches = command!()
+fn cli() -> Command {
+    command!()
         .name("dm")
         .about(t!("app.desc"))
         .subcommand(local::profile::args())
         .subcommand(local::group::args())
         .subcommand(info::args())
         .subcommand(local::file::args_add())
-        .get_matches_from(&mut wild::args_os());
-    None.or(local::profile::try_match(&matches).await)
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    apply_locales().await;
+    let matches = cli().get_matches_from(&mut wild::args_os());
+    let matched = None
+        .or(local::profile::try_match(&matches).await)
         .or(local::group::try_match(&matches).await)
         .or(local::file::try_match_add(&matches).await)
-        .or(info::try_match(&matches).await)
-        .unwrap()
+        .or(info::try_match(&matches).await);
+    if let None = matched {
+        return cli().print_long_help().into_diagnostic();
+    }else{
+        return matched.unwrap();
+    }
 }
