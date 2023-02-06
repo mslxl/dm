6ec6a90ef8576c16ff37181ff053c6ff4125e52b
mslxl <i@mslxl.com>
2023-02-06 20:18:51 +0800

    fix(cli): parse i32 fail in cli

--- src/ui/cli.rs
+++ src/ui/cli.rs
@@ -1,5 +1,6 @@
+use miette::{IntoDiagnostic, Result};
+use rust_i18n::t;
 use std::io::Write;
-use miette::{Result, IntoDiagnostic};
 
 use owo_colors::OwoColorize;
 
@@ -9,20 +10,43 @@ pub struct Cli;
 impl Ui for Cli {
     fn msg(&self, level: MsgLevel, msg: String) {
         match level {
-            MsgLevel::Error => print!("[{}] ", "Error".red()),
-            MsgLevel::Warn => print!("[{}] ", "Warn".yellow()),
-            MsgLevel::Info => print!("[{}] ", "Info".bright_white()),
+            MsgLevel::Error => print!("{} ", "[Error]".red()),
+            MsgLevel::Warn => print!("{} ", "[Warn]".yellow()),
+            MsgLevel::Info => print!("{} ", "[Info]".bright_white()),
         }
         println!("{}", msg)
     }
 
-    fn input(&self, prompt: Option<String>) -> Result<String> {
-      if let Some(msg) = prompt{
-        print!("{}: ", msg);
-      }
-      let mut input = String::new();
-      std::io::stdout().flush().into_diagnostic()?;
-      std::io::stdin().read_line(&mut input).into_diagnostic()?;
-      Ok(input)
+    fn input(&self, prompt: Option<&str>) -> Result<String> {
+        if let Some(msg) = prompt {
+            print!("{}: ", msg);
+        }
+        let mut input = String::new();
+        std::io::stdout().flush().into_diagnostic()?;
+        std::io::stdin().read_line(&mut input).into_diagnostic()?;
+        Ok(input)
+    }
+
+    fn choose(&self, prompt: Option<&str>, item: Vec<&str>) -> Result<i32> {
+        loop {
+            if item.len() > 10 {
+                for (idx, select) in item.iter().enumerate() {
+                    print!("[{:2}]: {}\t", idx.bright_white(), select);
+                }
+            } else {
+                for (idx, select) in item.iter().enumerate() {
+                    println!("{:2}. {}", idx.bright_white(), select);
+                }
+            }
+            let pos = self.input_i32(prompt)?;
+            if pos >= 0 && pos < item.len().try_into().unwrap() {
+                break Ok(pos);
+            } else {
+                self.msg(
+                    MsgLevel::Error,
+                    t!("error.prompt.missing_choose", pos = &pos.to_string()),
+                )
+            }
+        }
     }
 }
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -4,22 +4,23 @@ use rust_i18n::t;
 pub mod cli;
 pub trait Ui {
     fn msg(&self, level: MsgLevel, msg: String);
-    fn input(&self, prompt: Option<String>) -> Result<String>;
-    fn input_i32(&self, prompt: Option<String>) -> Result<i32> {
+    fn input(&self, prompt: Option<&str>) -> Result<String>;
+    fn choose(&self, prompt: Option<&str>, item: Vec<&str>)->Result<i32>;
+    fn input_i32(&self, prompt: Option<&str>) -> Result<i32> {
         loop {
             let text = self.input(prompt.clone())?;
-            if let Ok(num) = text.parse::<i32>() {
+            if let Ok(num) = text.trim().parse::<i32>() {
                 break Ok(num);
             } else {
                 self.msg(MsgLevel::Error, t!("error.prompt.nan"))
             }
         }
     }
-    fn input_yes_or_no(&self, prompt: Option<String>, default: bool) -> Result<bool> {
+    fn input_yes_or_no(&self, prompt: Option<&str>, default: bool) -> Result<bool> {
         let addition = if default { "Y/n" } else { "y/N" };
         let prompt = prompt.map(|v| format!("{} [{}]", v, addition));
         loop {
-            let input = self.input(prompt.clone())?.to_uppercase();
+            let input = self.input(prompt.as_ref().map(|x| &**x))?.to_uppercase();
             let text = input.trim();
             if text.is_empty() {
                 break Ok(default);
